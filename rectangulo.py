70f77e6195bdecf8913dc4b4a18ea4493d4423eb
Alejandro Fernandez <alejandrofemu@gmail.com>
2025-05-04 18:42:08 +0200

    segundo test ok

--- rectangulo.py
+++ rectangulo.py
@@ -1,5 +1,10 @@
 class Rectangle:
     def __init__(self,xmin,ymin,xmax,ymax):
+        if xmin >= xmax:
+            raise ValueError("Xmin debe ser menor que Xmax")
+        if ymin >= ymax:
+            raise ValueError("Ymin debe ser menor que Ymax")
+
         self.xmin = xmin
         self.ymin = ymin
         self.xmax = xmax
