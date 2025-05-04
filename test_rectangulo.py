2d325c6926a9b91a6f8b99bbd81fc0b54cd87ff7
Alejandro Fernandez <alejandrofemu@gmail.com>
2025-05-04 18:40:03 +0200

    segundo test falla

--- test_rectangulo.py
+++ test_rectangulo.py
@@ -8,3 +8,9 @@ class Test_Rectangle(unittest.TestCase):
         self.assertEqual(rectan.ymin,0)
         self.assertEqual(rectan.xmax,10)
         self.assertEqual(rectan.ymax,10)
+
+    def test_creation_invalid(self):
+        with self.assertRaises(ValueError):
+            Rectangle(10,0,0,10) # xmin > xmax --> debe fallar
+        with self.assertRaises(ValueError):
+            Rectangle(0,10,0,10) #ymin > ymax --> debe fallar
