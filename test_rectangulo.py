cc3135a1d9ae76b73c74da6a39eb4ed2e61c60ac
Alejandro Fernandez <alejandrofemu@gmail.com>
2025-05-04 19:44:10 +0200

    septimo test falla

--- test_rectangulo.py
+++ test_rectangulo.py
@@ -29,7 +29,19 @@ class Test_Rectangle(unittest.TestCase):
     def test_diagonal(self):
         rect = Rectangle(0,0,3,4)
         self.assertEqual(rect.diagonal(),5)
+    
+    def test_overlap(self):
+        rect1 = Rectangle(0,0,10,10)
+        rect2 = Rectangle(5,5,15,15)
+        rect3 = Rectangle(11,11,20,20)
 
+        self.assertTrue(rect1.overlaps(rect2))
+        self.assertTrue(rect2.overlaps(rect1))
+        self.assertTrue(rect2.overlaps(rect3))
+        self.assertTrue(rect3.overlaps(rect2))
+        self.assertFalse(rect1.overlaps(rect3))
+        self.assertFalse(rect3.overlaps(rect1))
+    
 
 
 
